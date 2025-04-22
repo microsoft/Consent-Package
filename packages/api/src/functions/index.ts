e9d0c3215280702f7c84c19f7ff482b7be0213c9
Stalgia Grigg <stalgia@bocoup.com>
2025-04-22 16:23:32 -0700

    Don't use composite

--- packages/api/src/functions/index.ts
+++ packages/api/src/functions/index.ts
@@ -1,7 +1,2 @@
-// Import all functions
-import "./standalone.js";
-import "./createConsent.js";
-
-// Export for build reference
-export * from "./standalone.js";
 export * from "./createConsent.js";
+export * from "./standalone.js";
